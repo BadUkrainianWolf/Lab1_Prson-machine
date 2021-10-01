a88b5a261d257e86dc855921563b0887aace38e4
BadWolf03 <tspam765@gmail.com>
2021-10-01 13:01:28 +0300

    Start Test1

---
+++ WpfApp1/Test1.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для Test1.xaml
+    /// </summary>
+    public partial class Test1 : Window
+    {
+        public Test1()
+        {
+            InitializeComponent();
+        }
+
+        private void Polygon_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Polygon_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
