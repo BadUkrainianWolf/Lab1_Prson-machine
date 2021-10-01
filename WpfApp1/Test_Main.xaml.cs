002158a3c2b90a82ba4fbaf49d07d0f2bbc51ce5
BadWolf03 <tspam765@gmail.com>
2021-10-01 12:24:13 +0300

    Test_Main

---
+++ WpfApp1/Test_Main.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для Test_Main.xaml
+    /// </summary>
+    public partial class Test_Main : Window
+    {
+        public Test_Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Polygon_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Test1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Test2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
